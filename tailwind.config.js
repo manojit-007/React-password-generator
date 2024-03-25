860bd0e18be215c3b584b6c11e65806127b0a708
manojit-007 <159749926+manojit-007@users.noreply.github.com>
2024-03-25 18:12:55 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// tailwind.config.js
+
